1d92e5a36f548438c34fccd5e3e2480eef51e0d7
LiLeoH <leo2012i@qq.com>
2018-10-12 15:48:07 +0800

    idle cpu usage optimized

--- cquery-adapter.go
+++ cquery-adapter.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"net"
 	"os"
-	"runtime"
 )
 
 func readStdinSendConn(tcpConn net.Conn, logPath *string) {
@@ -32,31 +31,6 @@ func readStdinSendConn(tcpConn net.Conn, logPath *string) {
 	}
 }
 
-func recvConnWriteStdout(tcpConn net.Conn, logPath *string) {
-	var buffer [6 * 1024 * 1024]byte
-	var flog *os.File
-	if logPath != nil && len(*logPath) > 0 {
-		flog, _ = os.OpenFile(*logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		defer flog.Close()
-	}
-
-	for {
-		n, err := tcpConn.Read(buffer[:len(buffer)-1])
-		if err != nil {
-			panic(err)
-		}
-
-		n, err = os.Stdout.Write(buffer[:n])
-		if err != nil {
-			panic(err)
-		}
-
-		if flog != nil {
-			flog.Write(buffer[:n])
-		}
-	}
-}
-
 func main() {
 	inLogPath := flag.String("inlog", "", "input message log file path")
 	outLogPath := flag.String("outlog", "", "output message log file path")
@@ -71,9 +45,29 @@ func main() {
 	defer tcpConn.Close()
 
 	go readStdinSendConn(tcpConn, inLogPath)
-	go recvConnWriteStdout(tcpConn, outLogPath)
 
-	for {
-		runtime.Gosched()
+	{
+		var buffer [6 * 1024 * 1024]byte
+		var flog *os.File
+		if len(*outLogPath) > 0 {
+			flog, _ = os.OpenFile(*outLogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			defer flog.Close()
+		}
+
+		for {
+			n, err := tcpConn.Read(buffer[:len(buffer)-1])
+			if err != nil {
+				panic(err)
+			}
+
+			n, err = os.Stdout.Write(buffer[:n])
+			if err != nil {
+				panic(err)
+			}
+
+			if flog != nil {
+				flog.Write(buffer[:n])
+			}
+		}
 	}
 }
